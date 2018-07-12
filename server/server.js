a98ec7458a3b7888553ed573a5dcd109b329c4f8
ratan1113 <lratan2052@gmail.com>
2018-07-12 15:16:38 +0545

    add connect and disconnect handeller using socket.io liberary

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const path=require('path');
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.io');
 
-const publicPath=path.join(__dirname,'../public');
+const publicPath=path. join(__dirname,'../public');
+const port=process.env.PORT||3000;
 // console.log(publicPath);
 // console.log(__dirname);
 // console.log(__dirname+'../public');
 
+//create an express object
 const app=express();
+
+//create a server using http node_modules
+var server=http.createServer(app);
+
+//create an IO variable to communicate client and server asynchronusly
+var io=socketIO(server);
 
 //setting a middleware to render public files:
 app.use(express.static(publicPath));
@@ -15,9 +25,17 @@ app.use(express.static(publicPath));
 // 	res.send('<h2>welcome to main page of the project</h2>');
 // });
 
+//io.on method let you register a event listener .which will called when the event is occure
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+
+	socket.on('disconnect',()=>{
+		console.log('user is disconnected');
+	});
+});
 
 
 
-app.listen(3000,()=>{
-	console.log('server started at port 3000');
+server.listen(port,()=>{
+	console.log(`server started at port ${port}`);
 });
